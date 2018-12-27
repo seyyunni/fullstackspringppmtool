08872607aea1df7fc34a31f23a609bf95e17dd6b
Eyyunni <seyyunni@LAMU02VV649HTD6.uhc.com>
2018-12-27 17:15:29 -0500

    full stack first commit

---
+++ src/test/java/com/skill/fullstackrun/FullstackrunApplicationTests.java
@@ -0,0 +1,17 @@
+package com.skill.fullstackrun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullstackrunApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
